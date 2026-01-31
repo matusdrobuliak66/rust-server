1e468a80bf2551ae41cafa5542105ac626342baf
Matus Drobuliak <drobuliak66@gmail.com>
2026-01-31 13:41:13 +0100

    improvement

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,10 +1,19 @@
 use super::server::Handler;
-use super::http::{Request, Response, StatusCode};
+use super::http::{Method, Request, Response, StatusCode};
 
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("Hello from WebsiteHandler!".to_string()))
+        match request.method() {
+            Method::GET => {
+                match request.path() {
+                    "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome to the homepage!</h1>".to_string())),
+                    "/about" => Response::new(StatusCode::Ok, Some("<h1>About us</h1><p>This is the about page.</p>".to_string())),
+                    _ => Response::new(StatusCode::NotFound, None),
+                }
+            }
+            _ => Response::new(StatusCode::NotFound, None),
+        }
     }
 }
